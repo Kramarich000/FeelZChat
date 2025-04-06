c87c8da27ca51f79308c31c9fa4638e78279943d
Karen <ipepela1@mail.ru~>
2025-04-07 02:57:58 +0300

    хедер анимируется мягко

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,33 @@
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { useState } from "react";
 
 export default function Header() {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoaded(true); 
+    }, 300); 
+
+    return () => clearTimeout(timer); 
+  }, []);
+
   return (
-    <header className="container mx-auto bg-amber-50 min-w-[100%] p-5 transition-all min-h-[100px]">
+    <header
+      className={`header container mx-auto bg-amber-50 min-w-[100%] p-5 transition-all min-h-[100px] ${isLoaded ? "loaded" : ""}`}
+    >
       <div className="max-w-[1200px] mx-auto flex justify-between items-center">
         <Link className="block w-10" to="/">
           <img className="" src="/images/logo.png" alt="" />
         </Link>
         <div className="">
-          <Link className="hover:underline text-cyan-700" to="/login">Вход</Link>
-          <Link className="hover:underline text-cyan-700" to="/register">/Регистрация</Link>
+          <Link className="hover:underline text-cyan-700" to="/login">
+            Вход
+          </Link>
+          <Link className="hover:underline text-cyan-700" to="/register">
+            /Регистрация
+          </Link>
         </div>
       </div>
     </header>
