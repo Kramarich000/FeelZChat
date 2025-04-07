b320add6c754a1d967f06a94ffc2cd4643dc99d9
Karen <ipepela1@mail.ru~>
2025-04-07 23:31:44 +0300

    убрал работу с токенами временно

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,28 +1,28 @@
 import axios from "axios";
-import { refreshAccessToken } from "./refreshAccessToken";
+// import { refreshAccessToken } from "./refreshAccessToken";
 
 const api = axios.create({
   baseURL: "https://signalforge.onrender.com/api/v1",
 });
 
 api.interceptors.request.use(async (config) => {
-  let token = localStorage.getItem("access_token");
-  const expiresAt = Number(localStorage.getItem("expires_at"));
+  // let token = localStorage.getItem("access_token");
+  // const expiresAt = Number(localStorage.getItem("expires_at"));
 
-  if (Date.now() >= expiresAt) {
-    try {
-      token = await refreshAccessToken();
-    } catch (e) {
-      console.error("Failed to refresh token", e);
-      localStorage.clear();
-      window.location.href = "/login";
-      return Promise.reject(e);
-    }
-  }
+  // if (Date.now() >= expiresAt) {
+  //   try {
+  //     token = await refreshAccessToken();
+  //   } catch (e) {
+  //     console.error("Failed to refresh token", e);
+  //     localStorage.clear();
+  //     window.location.href = "/login";
+  //     return Promise.reject(e);
+  //   }
+  // }
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+  // if (token) {
+  //   config.headers.Authorization = `Bearer ${token}`;
+  // }
 
   return config;
 });
