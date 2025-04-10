ea2644ae4d1ec01c3f37dbd3f0db1d010cf2041e
Karen <ipepela1@mail.ru~>
2025-04-11 00:00:57 +0300

    ну вот сейчас давай

--- vite.config.js
+++ vite.config.js
@@ -17,8 +17,8 @@ export default defineConfig({
       algorithm: 'brotliCompress',
       ext: '.br',
     }),
-    VitePWA(),
-    analyzer(),
+    // VitePWA(),
+    // analyzer(),
     // ViteImageOptimizer ({
     //   jpeg: {
     //     quality: 75,
