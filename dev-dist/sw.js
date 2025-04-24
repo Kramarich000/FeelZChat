a149ca80748e4b4486827f4f9962f48c72a3ea1c
Karen <ipepela1@mail.ru~>
2025-04-24 20:09:40 +0300

    новое фото в метаданных

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-fc76d6c3'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0qooki084i8"
+    "revision": "0.inemdfj88cg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
