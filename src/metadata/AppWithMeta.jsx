8981887e1b9178f98ea77f8c8bf3f6152e99eeba
Karen <ipepela1@mail.ru~>
2025-04-24 20:25:42 +0300

    дальнейшня работа с метатегами

--- src/metadata/AppWithMeta.jsx
+++ src/metadata/AppWithMeta.jsx
@@ -1,19 +1,22 @@
-import { useEffect } from 'react';
-import App from '../App';
-import FZ from '../assets/images/FZ.jpg'
+import { useEffect } from "react";
+import App from "../App";
+
 export default function AppWithMeta() {
-    useEffect(() => {
-      document.title = "FeelZChat";  
-      
-      const metaTitle = document.querySelector('meta[property="og:title"]');
-      const metaDescription = document.querySelector('meta[property="og:description"]');
-      const metaImage = document.querySelector('meta[property="og:image"]');
-      
-      if (metaTitle) metaTitle.setAttribute("content", "FeelZChat");
-      if (metaDescription) metaDescription.setAttribute("content", "A modern messaging app for seamless communication.");
-      if (metaImage) metaImage.setAttribute("content", FZ);
-  
-    }, []);  
-  
-    return <App />;
-  }
+  useEffect(() => {
+    document.title = "FeelZChat";
+
+    const metaTitle = document.querySelector('meta[property="og:title"]');
+    const metaDescription = document.querySelector('meta[property="og:description"]');
+    const metaImage = document.querySelector('meta[property="og:image"]');
+
+    if (metaTitle) metaTitle.setAttribute("content", "FeelZChat");
+    if (metaDescription)
+      metaDescription.setAttribute("content", "A modern messaging app for seamless communication.");
+
+    const imageURL = `${window.location.origin}/src/assets/images/FZ.jpg`;
+
+    if (metaImage) metaImage.setAttribute("content", imageURL);
+  }, []);
+
+  return <App />;
+}
