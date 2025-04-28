6d8812bba2cb87acdbefb448082d62c7ba3c7fde
Karen <ipepela1@mail.ru~>
2025-04-28 18:34:56 +0300

    скобки)

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import BgGradient from "@components/BgGradient";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
--- src/pages/errors/500.jsx
+++ src/pages/errors/500.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BsFillExclamationDiamondFill } from "react-icons/bs";
 import translate from "@utils/translate";
 
