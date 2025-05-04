c0f596c8a46a5d0849948a798a002b6b71d5f604
Karen <ipepela1@mail.ru~>
2025-05-04 23:26:23 +0300

    теперь точно верно вынес)

--- public/extra-head.js
+++ public/extra-head.js
@@ -26,6 +26,34 @@ setTimeout(() => {
         });
 }, 2000);
 
+setTimeout(() => {
+    fetch('https://analytics.messenger-app-movb.onrender.com//gtag/js?id=G-XXXXXXX', {
+        method: 'GET',
+        headers: {
+            'Cache-Control': 'no-cache',
+        },
+    })
+        .then(res => {
+            if (!res.ok) {
+                throw new Error('Ошибка загрузки скрипта');
+            }
+            return res.text();
+        })
+        .then(code => {
+            const script = document.createElement('script');
+            script.textContent = code;
+            document.head.appendChild(script);
+
+            window.dataLayer = window.dataLayer || [];
+            function gtag() { dataLayer.push(arguments); }
+            gtag('js', new Date());
+            gtag('config', 'G-XXXXXXX');
+        })
+        .catch(error => {
+            console.error('Не удалось загрузить Google Analytics:', error);
+        });
+}, 2000);
+
 (function () {
     var ld = document.createElement('script');
     ld.type = 'application/ld+json';
