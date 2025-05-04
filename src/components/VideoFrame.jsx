435b52945f9c6cbd3a4b6cfc5498aa7086bf210e
Karen <ipepela1@mail.ru~>
2025-05-04 20:52:55 +0300

    добавил фасад на видео

--- src/components/VideoFrame.jsx
+++ src/components/VideoFrame.jsx
@@ -1,20 +1,27 @@
 import { useEffect, useRef, useState } from 'react';
 import { motion } from 'framer-motion';
 import { SafeMotion } from '@components/SafeMotion';
+import { FaYoutube } from 'react-icons/fa';
+
 export default function VideoFrame() {
   const [failed, setFailed] = useState(false);
   const [loaded, setLoaded] = useState(false);
   const timerRef = useRef(null);
+  const [loadingStarted, setLoadingStarted] = useState(false);
 
-  useEffect(() => {
-    timerRef.current = setTimeout(() => {
-      if (!loaded) {
-        setFailed(true);
-      }
-    }, 60000);
+  const handlePlayClick = () => {
+    setLoadingStarted(true);
+  };
 
-    return () => clearTimeout(timerRef.current);
-  }, [loaded]);
+  // useEffect(() => {
+  //   timerRef.current = setTimeout(() => {
+  //     if (!loaded) {
+  //       setFailed(true);
+  //     }
+  //   }, 60000);
+
+  //   return () => clearTimeout(timerRef.current);
+  // }, [loaded]);
 
   return (
     <SafeMotion
@@ -25,37 +32,60 @@ export default function VideoFrame() {
     >
       {failed ? (
         <div className="w-full h-full flex items-center justify-center bg-black text-center">
-          <p className="text-primary text-sm sm:text-4xl  font-bold w-[700px]">
+          <p className="text-primary text-sm sm:text-4xl font-bold w-[700px]">
             Видео недоступно. Проверьте подключение к интернету или попробуйте
             позже.
           </p>
         </div>
       ) : (
-        <SafeMotion
-          as="iframe"
-          sandbox="allow-scripts allow-same-origin allow-presentation"
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1 }}
-          // transition={{ delay: 0.5, duration: 0.5 }}
-          className="w-full h-[250px] sm:h-[300px] md:h-[500px]"
-          src="https://www.youtube.com/embed/-U8rukzWCNs"
-          title="Видео"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-          loading="lazy"
-          onLoad={() => {
-            clearTimeout(timerRef.current);
-            setLoaded(true);
-          }}
-          style={{
-            visibility: loaded ? 'visible' : 'hidden',
-            opacity: loaded ? 1 : 0,
-            transition: 'opacity 0.5s ease',
-          }}
-        />
+        <>
+          {!loadingStarted && (
+            <div
+              className="w-full h-full flex items-center justify-center bg-black "
+              onClick={handlePlayClick}
+            >
+              <div className="flex flex-col items-center cursor-pointer">
+                <img
+                  src="https://img.youtube.com/vi/-U8rukzWCNs/hqdefault.jpg"
+                  alt="Video thumbnail"
+                  className="w-full h-[250px] sm:h-[300px] md:h-[500px] object-contain"
+                />
+                <FaYoutube
+                  className="absolute top-[45%] "
+                  size={75}
+                  color="red"
+                />
+              </div>
+            </div>
+          )}
+          {loadingStarted && (
+            <SafeMotion
+              as="iframe"
+              src="https://www.youtube.com/embed/-U8rukzWCNs?autoplay=1"
+              sandbox="allow-scripts allow-same-origin allow-presentation"
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              className="w-full h-[250px] sm:h-[300px] md:h-[500px]"
+              title="Видео"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+              loading="lazy"
+              onLoad={() => {
+                clearTimeout(timerRef.current);
+                setLoaded(true);
+              }}
+              style={{
+                visibility: loaded ? 'visible' : 'hidden',
+                opacity: loaded ? 1 : 0,
+                transition: 'opacity 0.5s ease',
+              }}
+            />
+          )}
+        </>
       )}
-      {!loaded && !failed && (
+
+      {!loaded && loadingStarted && !failed && (
         <div className="absolute inset-0 flex items-center justify-center bg-black animate-pulse">
           <span className="text-primary text-2xl sm:text-4xl font-bold">
             Загрузка видео...
